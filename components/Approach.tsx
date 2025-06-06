31bd2131a8245b20f75f0e4fb6becb04612b19dc
Sumit Singh kaintura <98005540+sumitkainturaji@users.noreply.github.com>
2025-06-06 18:05:27 +0530

    Update Approach.tsx

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -69,11 +69,6 @@ const Card = ({
       onMouseLeave={() => setHovered(false)}
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem]  lg:h-[35rem] rounded-3xl"
     >
-      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
-
       <AnimatePresence>
         {hovered && (
           <motion.div
